83359c0fcd3714f77a5d0bf6f1a26857f36dd680
Sammy Calle <sammycallet@gmail.com>
2025-03-07 23:11:37 +0200

    WebSocket Implemented

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,5 +31,6 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("io.ktor:ktor-client-content-negotiation-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,21 +1,62 @@
 package sammy.ktor.tutorials
 
-import io.ktor.client.request.*
-import io.ktor.http.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.serialization.*
+import io.ktor.serialization.kotlinx.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlinx.coroutines.flow.*
+import kotlinx.serialization.json.Json
+import sammy.ktor.tutorials.model.Priority
+import sammy.ktor.tutorials.model.Task
+import kotlin.test.*
 
 class ApplicationTest {
-
     @Test
     fun testRoot() = testApplication {
         application {
-            module()
+            configureRouting()
+            configureSerialization()
+            configureSockets()
         }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
+
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+            install(WebSockets) {
+                contentConverter =
+                    KotlinxWebsocketSerializationConverter(Json)
+            }
+        }
+
+        val expectedTasks = listOf(
+            Task("cleaning", "Clean the house", Priority.Low),
+            Task("gardening", "Mow the lawn", Priority.Medium),
+            Task("shopping", "Buy the groceries", Priority.High),
+            Task("painting", "Paint the fence", Priority.Medium)
+        )
+        var actualTasks = emptyList<Task>()
+
+        client.webSocket("/tasks") {
+            consumeTasksAsFlow().collect { allTasks ->
+                actualTasks = allTasks
+            }
+        }
+
+        assertEquals(expectedTasks.size, actualTasks.size)
+        expectedTasks.forEachIndexed { index, task ->
+            assertEquals(task, actualTasks[index])
         }
     }
 
+    private fun DefaultClientWebSocketSession.consumeTasksAsFlow() = incoming
+        .consumeAsFlow()
+        .map {
+            converter!!.deserialize<Task>(it)
+        }
+        .scan(emptyList<Task>()) { list, task ->
+            list + task
+        }
 }
